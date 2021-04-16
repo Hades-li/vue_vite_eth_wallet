d120fccb83884d1d8249e231cda00a7bcef6c943
ignis <hades.jiali@gmail.com>
2021-04-16 23:27:04 +0800

    build阶段去除eslint检测

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import {defineConfig} from 'vite'
+import {defineConfig, mergeConfig, UserConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from '@rollup/plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default {
   plugins: [
     vue(),
     eslint()
-    /*{
-      ...eslint(),
-      enforce: 'pre',
-      apply: 'build',
-    },*/
   ]
+}*/
+export default defineConfig(({mode}) => {
+  let config: UserConfig = {
+    plugins: [
+      vue()
+    ]
+  }
+
+  if (mode === 'development') {
+   config = mergeConfig(config, {
+      plugins: [
+        eslint()
+      ]
+    }, true)
+  } else {
+    config = mergeConfig(config, {}, true)
+  }
+
+  return config
 })
